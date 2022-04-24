d1463443ea332104329202e7fbfcc4818ca66443
adhinabraham <adhinabraham2000@gmail.com>
2022-04-24 10:41:23 +0530

    static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -201,14 +202,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/as/'
-# STATICFILES_DIRS=[
-#     BASE_DIR/'pics'
+STATICFILES_DIRS=[
+   os.path.join(BASE_DIR,"static")
 
+]
 
-# ]
-
-
-MEIDA_ROOT = '/pics/'
+STATIC_ROOT= os.path.join(BASE_DIR,"staticfiles")
+MEDIA_ROOT = '/pics/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
